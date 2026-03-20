261dfe228f28d6383861966a267d028ad5ad6fb3
AINDUSTRIES <contact.aindustries@gmail.com>
2026-03-20 15:18:59 +0100

    Implémentation de placeholder pour les autres pages

---
+++ app/pages/keyboard.py
@@ -0,0 +1,26 @@
+from PySide6.QtWidgets import QGridLayout, QWidget, QPushButton, QLabel
+from PySide6.QtCore import Qt
+from . import menu # Import du package menu entier pour éviter une erreur d'import circulaire
+
+
+class Keyboard(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+
+        # Instanciation du layout
+        self.layout = QGridLayout(self)
+
+        # Instanciation des boutons
+        self.homeButton = QPushButton("menu") # A changer pour une icone
+        
+        self._test = QLabel("Clavier")
+        self._test.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Connection des events
+        self.homeButton.clicked.connect(lambda _event: self.switch(parent, menu.Menu(parent)))
+
+        self.layout.addWidget(self._test, 0, 0)
+        self.layout.addWidget(self.homeButton, 1, 0)
+
+    def switch(self, parent, page):
+        parent.inner = page
