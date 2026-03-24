3f5cad913feb1a6864f917d6f9e423f48ce8c2de
AINDUSTRIES <contact.aindustries@gmail.com>
2026-03-24 08:40:51 +0100

    Ajout des paramètres dans l'interface

--- app/pages/settings.py
+++ app/pages/settings.py
@@ -1,26 +1,61 @@
-from PySide6.QtWidgets import QGridLayout, QWidget, QPushButton, QLabel
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QPushButton, QLabel, QHBoxLayout, QVBoxLayout
+from PySide6.QtCore import Qt, Signal, QObject
+from ..modules.settings import SettingsManager
 
 class Settings(QWidget):
     def __init__(self, parent):
         super().__init__()
 
         # Instanciation du layout
-        self.layout = QGridLayout(self)
+        self.layout = QVBoxLayout(self)
 
         # Instanciation des widgets
         self.homeButton = QPushButton("menu") # A changer pour une icone
-
-        self._test = QLabel("Paramètres")
-        self._test.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.voiceChoice = Setting("voice", "Voix du text-to-speech", ["Femme", "Homme"], [0, 1])
 
         # Connection des events
         self.homeButton.clicked.connect(lambda _event: self.switch(parent))
 
-        self.layout.addWidget(self._test, 0, 0)
-        self.layout.addWidget(self.homeButton, 1, 0)
+        self.layout.addWidget(self.voiceChoice)
+        self.layout.addStretch(1)
+        self.layout.addWidget(self.homeButton)
 
     def switch(self, parent):
         from .menu import Menu
 
         parent.inner = Menu(parent)
+
+class Setting(QWidget):
+    def __init__(self, name, description, options, values):
+        super().__init__()
+        self.name = name
+        self.values = values
+
+        self.layout = QHBoxLayout(self)
+
+        self.description = QLabel(description)
+
+        self.layout.addWidget(self.description)
+        self.layout.addStretch(1)
+
+        self.buttons = []
+        for i, option in enumerate(options):
+            button = QPushButton(option)
+            button.clicked.connect(lambda _event, value=values[i]: self.setValue(value))
+            self.layout.addWidget(button)
+            self.buttons.append(button)
+        
+        self.updateButtons()
+
+    def setValue(self, value):
+        SettingsManager().setSetting(self.name, value)
+        self.updateButtons()
+
+    def updateButtons(self):
+        currentSettingValue = SettingsManager().getSetting(self.name)
+
+        for i, button in enumerate(self.buttons):
+            if self.values[i] == currentSettingValue:
+                button.setStyleSheet("color:red;")
+            else:
+                button.setStyleSheet("")
