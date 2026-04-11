8520983f625dd604c62c825bd0e2ab32789d41c9
AINDUSTRIES <contact.aindustries@gmail.com>
2026-04-11 21:56:22 +0200

    Created contact page

---
+++ app/pages/contact.py
@@ -0,0 +1,22 @@
+from PySide6.QtWidgets import QGridLayout, QPushButton, QLabel
+from PySide6.QtCore import Qt
+from . import Page
+
+class Contact(Page):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        # Instanciation du layout
+        self.layout = QGridLayout(self)
+
+        # Instanciation des boutons
+        self.homeButton = QPushButton("retour") # A changer pour une icone
+        
+        self._test = QLabel("Contact")
+        self._test.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Connection des events
+        self.homeButton.clicked.connect(lambda _: self.switch("contacts"))
+
+        self.layout.addWidget(self._test, 0, 0)
+        self.layout.addWidget(self.homeButton, 1, 0)
