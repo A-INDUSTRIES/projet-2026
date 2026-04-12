26fa62c616cc345c46970874c1e5eba5a1da8e01
AINDUSTRIES <contact.aindustries@gmail.com>
2026-04-13 00:00:32 +0200

    Implémentation de l'interface des contacts

--- app/modules/contacts.py
+++ app/modules/contacts.py
@@ -4,6 +4,25 @@ from .utils import Singleton, getUserDataPath
 def getContactsPath():
     return getUserDataPath() / "contacts.json"
 
+class Contact():
+    def __init__(self, initial=None):
+        if initial==None:
+            self.name = ""
+            self.email = ""
+        else:
+            self.name = initial["name"]
+            self.email = initial["email"]
+
 class ContactsManager(metaclass=Singleton):
     def __init__(self):
-        pass
+        self.contactsPath = getContactsPath()
+        if not self.contactsPath.exists():
+            self.contactsPath.parent.mkdir(exist_ok=True)
+            with open(self.contactsPath, 'w') as f:
+                json.dump([], f)
+        
+        with open(self.contactsPath, "r") as f:
+            self.contacts = list(map(Contact, json.load(f)))
+
+    def getContacts(self):
+        return self.contacts
--- app/pages/contacts.py
+++ app/pages/contacts.py
@@ -1,5 +1,7 @@
-from PySide6.QtWidgets import QGridLayout, QPushButton, QLabel
-from PySide6.QtCore import Qt
+from PySide6.QtGui import QPainter
+from PySide6.QtWidgets import QHBoxLayout, QLabel, QPushButton, QScrollArea, QSizePolicy, QStyle, QStyleOption, QVBoxLayout, QWidget
+from app.modules.contacts import Contact as C
+from app.modules.contacts import ContactsManager
 from . import Page
 
 class Contacts(Page):
@@ -7,16 +9,57 @@ class Contacts(Page):
         super().__init__(*args)
 
         # Instanciation du layout
-        self.layout = QGridLayout(self)
+        self.layout = QVBoxLayout(self)
 
-        # Instanciation des boutons
+        # Instanciation des widgets
+        self.title = QLabel("Contacts")
         self.homeButton = QPushButton("menu") # A changer pour une icone
-        
-        self._test = QLabel("Contacts")
-        self._test.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.bottomRow = QHBoxLayout()
+        self.contacts = QScrollArea()
+
+        self.contacts.setLayout(QVBoxLayout())
+        self.contacts.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Expanding)
 
         # Connection des events
-        self.homeButton.clicked.connect(lambda _: self.switch("menu"))
+        self.homeButton.clicked.connect(lambda _event: self.switch("menu"))
 
-        self.layout.addWidget(self._test, 0, 0)
-        self.layout.addWidget(self.homeButton, 1, 0)
+        test = Contact(C({"name": "test", "email": "test@gmail.com"}))
+        self.contacts.layout().addWidget(test)
+
+        for contact in ContactsManager().getContacts():
+            widget = Contact(contact)
+            self.contacts.layout().addWidget(widget)
+
+        self.createContact = QPushButton("Ajouter un contact")
+        self.contacts.layout().addWidget(self.createContact)
+        self.contacts.layout().addStretch(1)
+
+        self.layout.addWidget(self.title)
+        self.layout.addWidget(self.contacts)
+        self.bottomRow.addWidget(self.homeButton)
+        self.bottomRow.addStretch(1)
+        self.layout.addLayout(self.bottomRow)
+
+class Contact(QWidget):
+    def __init__(self, contact):
+        super().__init__()
+        self.name = QLabel(contact.name)
+        self.email = QLabel(contact.email)
+        self.edit = QPushButton("modifier")
+        self.delete = QPushButton("supprimer")
+
+        self.layout = QHBoxLayout(self)
+        self.info = QVBoxLayout()
+
+        self.info.addWidget(self.name)
+        self.info.addWidget(self.email)
+        self.layout.addLayout(self.info)
+        self.layout.addStretch(1)
+        self.layout.addWidget(self.edit)
+        self.layout.addWidget(self.delete)
+
+    def paintEvent(self, _):
+        opt = QStyleOption()
+        opt.initFrom(self)
+        p = QPainter(self)
+        self.style().drawPrimitive(QStyle.PrimitiveElement.PE_Widget, opt, p, self)
