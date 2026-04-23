448d989690a68e535c0e9dd11b6b35130114c69f
AINDUSTRIES <contact.aindustries@gmail.com>
2026-04-23 20:23:31 +0200

    Correctoin d'un bug lié à la librairie système espeak

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from PySide6.QtWidgets import QApplication
 from app.app import MainWindow
-from app.modules.logger import setLogLevel
+from app.modules.logger import setLogLevel, debug
+from ctypes.util import find_library
+import os
 
 if __name__ == "__main__":
     setLogLevel("debug")
+    espeak_path = find_library("espeak-ng") or find_library("espeak")
+    debug(espeak_path)
+    if espeak_path:
+        os.environ["PHONEMIZER_ESPEAK_LIBRARY"] = espeak_path
     app = QApplication()
     main_window = MainWindow()
     app.exec()
